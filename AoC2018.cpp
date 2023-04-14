82336cee3c9473384929bafd03f3662227b5e167
KWXPL <m1a1osowa@gmail.com>
2023-04-14 20:07:54 +0200

    Zadanie 1 dzien 3

---
+++ AoC2018.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class house 
+{
+	public:
+	int x = 0;
+	int y = 0;
+
+	house(int _x, int _y) : x(_x), y(_y) {}
+
+	bool operator==(const house& other) const 
+	{
+		return (x == other.x && y == other.y);
+	}
+};
+
+struct hasz
+{
+	size_t operator()(const house& obj) const
+	{
+		size_t h = 0;
+		hash<int> hashX;
+		hash<int> hashY;
+		h ^= hashX(obj.x);
+		h ^= hashY(obj.y);
+		return h;
+	}
+};
+
+int main()
+{
+	int x = 0;
+	int y = 0;
+	ifstream input{"Dane.txt"};
+	vector<char> inputData;
+	unordered_set<house, hasz> houses;
+	char data;
+	cout << "Start programu\n";
+	while (input >> data)
+	{
+		inputData.push_back(data);
+	}
+	cout << "Wielkosc danych wejsciowych " << inputData.size() << "\n";
+	houses.insert(house(x, y));
+	for (int i = 0; i < inputData.size(); i++)
+	{
+		switch (inputData[i])
+		{
+		case '<':
+			x--;
+			houses.insert(house(x, y));
+			cout << "< : " <<  houses.size() << "\n";
+			break;
+		case '>':
+			x++;
+			houses.insert(house(x, y));
+			cout << "> : " <<  houses.size() << "\n";
+			break;
+		case '^':
+			y++;
+			houses.insert(house(x, y));
+			cout << "^ : " <<  houses.size() << "\n";
+			break;
+		case 'v':
+			y--;
+			houses.insert(house(x, y));
+			cout << "V : " <<  houses.size() << "\n";
+			break;
+		default:
+			break;
+		}
+	}
+	cout << "Ilosc domow: " << houses.size() << "\n";
+}
